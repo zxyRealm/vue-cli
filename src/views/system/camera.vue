e2a9337997021601a3636ddfa02090febcb63269
刃下心 <1936689502@qq.com>
2020-04-25 23:48:21 +0800

    feat: add webcamjs demo

--- src/views/system/camera.vue
+++ src/views/system/camera.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <h1>系统摄像头拍照功能调研</h1>
+    <!-- <form id="web-camera">
+      <input type="button" value="拍照" @click="startCapture">
+    </form> -->
     <div id="web-camera" class="web-camera--wrap"></div>
-    <!-- <web-cam @error="handleCamError" ref="webCam"></web-cam> -->
     <div class="handle--wrap">
       <el-button @click="startCapture">拍照</el-button>
     </div>
+    <img v-if="dataUrl" :src="dataUrl" alt="">
   </div>
 </template>
 
@@ -18,9 +21,13 @@ export default {
   data () {
     return {
       cameraConfig: { // webcam 配置信息
-        width: 500,
-        height: 300,
-        image_format: 'jpg',
+        width: 320,
+        height: 240,
+        dest_width: 320,
+        dest_height: 240,
+        crop_width: 320, 
+        crop_height: 240,
+        image_format: 'jpeg',
         jpeg_quality: 90
       },
       dataUrl: '' // 抓拍图 base64 编码
@@ -33,23 +40,33 @@ export default {
   methods: {
     // 初始化
     initCamera () {
-      WebCam.reset()
+      this.destoryCamera()
       WebCam.on('error', (error) => {
         console.error('webcam error', error.message)
+      })
+      WebCam.on('live', data => {
+        console.log('camerar is alreay')
       })
       WebCam.set(this.cameraConfig)
       WebCam.attach('#web-camera')
     },
+    // 销毁 camera
+    destoryCamera () {
+      WebCam.reset()
+    },
     // 开始抓拍
     startCapture () {
       WebCam.snap(data => {
-        this.dataUrl = data.replace('', '')
+        this.dataUrl = data
         console.log('data', data)
       })
     },
     handleCamError (error) {
       console.error('error', error)
     }
+  },
+  beforeDestroy () {
+    this.destoryCamera()
   }
 }
 </script>
@@ -57,8 +74,6 @@ export default {
 <style lang="scss" scoped>
 .web-camera {
   &--wrap {
-    width: 500px;
-    height: 300px;
     margin: 10px 0;
     border: 1px solid $border-color;
     border-radius: 3px;
